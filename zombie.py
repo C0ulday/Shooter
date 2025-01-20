888c998c5eced09ad3e6d3ca61fc15424970625d
C#ita <120424147+C0ulday@users.noreply.github.com>
2025-01-20 13:13:51 +0100

    Add files via upload

---
+++ zombie.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Zombie(pygame.sprite.Sprite):
+    def __init__(self,x,y,velocity,image_path):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.velocity = velocity
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
+    
+def launch_projectile(image_path):
