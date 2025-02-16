dbb0b014f88aedce14351b18d279b722808a671b
ms-itachii <ismail.leomessi10@gmail.com>
2025-02-16 12:00:29 +0100

    Server launch a game

--- reseau/client.py
+++ reseau/client.py
@@ -1,13 +1,16 @@
 import socket
-import RPi.GPIO as GPIO
 import time
 import struct
+import RPi.GPIO as GPIO 
+import pickle as pkl
+from game import jeu
 
 class Client:
-    def __init__(self, ip_adress="localhost", port=4000, pin=13): # pin et ip adress à changer
+    def __init__(self, ip_adress="localhost", port=4000, pin=13, mode=1): # pin, ip adress et mode à changer
         self.ip_adress = ip_adress
         self.port = port
         self.pin = pin
+        self.mode = mode
 
         # Configuration du GPIO
         GPIO.setmode(GPIO.BCM)  
@@ -18,20 +21,19 @@ class Client:
         try:
             self.client_socket.connect((self.ip_adress, self.port))
             while True:
-                """Surveille le bouton et envoie un message lorsqu'il est pressé"""
                 print("En attente d'un appui sur le bouton...")
+
                 if GPIO.input(self.pin) == GPIO.HIGH:
                     print("Bouton pressé ! Envoi du message...")
                     message = "Send game"
-                    self.sendMessage()
-                    x_cible,y_cible = self.receiveData()
-                    # TODO : traitement d'image
-                    if True:
-                        message = "hit"
-                    else:
-                        message = "miss"
+                    self.sendMessage(message)
+                    message =  self.receiveData()
+                    if (message == "error"):
+                        print("Erreur dans Désérialisation...")
+                        break
 
-                    self.send_message(message)
+                    # dire au serveur si c'est un hit ou miss
+                    self.sendMessage(message) 
                     time.sleep(0.5)  # éviter les répétitions involontaires
                     break
 
@@ -40,24 +42,44 @@ class Client:
 
         finally:
             self.client_socket.close()
+            GPIO.cleanup() 
 
     def sendMessage(self, message):
         self.client_socket.sendall(message.encode("utf-8"))
-        response = self.client_socket.recv(1024).decode("utf-8")
-        print(f"Réponse du serveur : {response}")
-
-    def receiveMessage(self):
-        response = self.client_socket.recv(1024).decode("utf-8")
-        print(f"Réponse du serveur : {response}")
-        return response
+        print(f"Message envoyé: {message}")
 
     def receiveData(self):
-        data = self.client_socket.recv(1024)
-        print(f"Réponse du serveur : {data}")
-        x, y = struct.unpack("ii", data)
-        print(f"Decoded coordinates: x={x}, y={y}")
-        return x,y
+        # receive the size of the object that we should receive (4 bytes)
+        size_data = self.client_socket.recv(4)
+        if not size_data:
+            return "error"
 
+        expected_size = struct.unpack("I", size_data)[0]  # "I" format means unsigned integer
+        data = b""
+        
+        # loop to receive all data
+        while len(data) < expected_size:
+            packet = self.client_socket.recv(4096)
+            if not packet:
+                break
+            data += packet
+
+        try:
+            game = pkl.loads(data)  # Désérialisation
+            hit = False
+            if isinstance(game, jeu.Jeu):
+                print("Using method 2 to determine if it's a hit or not...")
+                # TODO : traitement d'image mode 2, changement de valeur de hit
+            else:
+                print("Using method 1 to determine if it's a hit or not...")
+                # TODO : traitement d'image mode 1, changement de valeur de hit
+            return "hit" if hit else "miss"
+        
+        except Exception as e:
+            print(f"Erreur de désérialisation : {e}")
+            return "error"  # Default return
+        
+        
 if __name__ == "__main__":
-    client = Client(ip_adress="")  
-    client.run()
+    client = Client()  
+    client.client()
--- reseau/server.py
+++ reseau/server.py
@@ -1,5 +1,8 @@
 import socket
-import pickle
+import pygame
+import struct
+import threading
+import pickle as pkl
 from game import jeu  
 
 
@@ -7,44 +10,34 @@ class server:
     def __init__(self):
         self.Ip_adress = 'localhost'
         self.Port = 4000
-        self.server()
+        self.clients = 0
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.bind((self.Ip_adress, self.Port))
+        self.serverSocket.listen(5)  # Connections multiples (spectateurs) 
+        print("Serveur en attente de connexion...")
+        
     
     def server(self):
-        # Création d'un socket
-        serveur_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # Association du socket à l'adresse locale et au port 4000
-        serveur_socket.bind((self.Ip_adress, self.Port))
-
-        serveur_socket.listen(1)
-        print("Serveur en attente de connexion...")
-
         try:
-            # Accepte une connexion entrante
-            connexion, adresse = serveur_socket.accept()
-            print(f"Connexion établie avec : {adresse}")
-            
+            # Start game in a separate thread
+            gameThread = threading.Thread(target=self.runGame, daemon=True)
+            gameThread.start()
+
             while True:
-                try:
-                    data = connexion.recv(1024)
-                    if not data:
-                        print("Aucune donnée reçue. Fermeture de la connexion.")
-                        break  # Sort de la boucle si aucune donnée n'est reçue
+                # Accepte une connexion entrante
+                connexion, adresse = self.serverSocket.accept()
+                print(f"Connexion établie avec : {adresse}")
+                self.clients += 1
 
-                    received_object = pickle.loads(data)  # Désérialisation
-                    
-                    # Vérification du type
-                    if isinstance(received_object, jeu.Jeu):
-                        print(f"Objet reçu !")
+                # Handle client
+                if (self.clients == 0):
+                    clientThread = threading.Thread(target=self.handleClient, args=(connexion, adresse), daemon=True)
+                    clientThread.start()
 
-                    else:
-                        print("Erreur : l'objet reçu n'est pas une instance de Jeu.")
-
-                except pickle.UnpicklingError:
-                    print("Erreur lors de la désérialisation des données.")
-                except socket.error as e:
-                    print(f"Erreur socket pendant la réception des données : {e}")
-                    break 
+                # Handle spectators
+                elif (self.clients > 0):
+                    spectatorThread = threading.Thread(target=self.handleSpectator, args=(connexion, adresse), daemon=True)
+                    spectatorThread.start()
 
         except socket.error as e:
             print(f"Erreur socket : {e}")
@@ -52,18 +45,55 @@ class server:
         finally:
             print("Fermeture des connexions...")
             connexion.close()
-            serveur_socket.close()
+            self.serverSocket.close() 
 
-    def sendObject(connexion, message):
-        # Envoyer un message ou un object au client
-        connexion.sendall(message.encode("utf-8"))
-        print(f"Message envoyer au client : {message}\n")
-        
-    def receiveObjct(connexion) :
-        # Réception du message du client
-        message_client = connexion.recv(1024).decode("utf-8")
-        print(f"Message reçu du client : {message_client}")
-        return message_client
+    def handleClient(self,connexion,adresse):
+        try:
+            while True:
+                data = connexion.recv(1024).decode("utf-8")
+                if (data == "Send game"):
+                    self.sendData(connexion, self.game)
+
+                elif (data == "miss"):
+                    print("Shot missed the target ...")
+                    #TODO
+
+                elif (data == "hit"):
+                    print("Shot hit the target ...")
+                    #TODO
+
+        except socket.error as e:
+            print(f"Erreur socket : {e}")
+
+        finally:
+            print("Fermeture de la connexion...")
+            connexion.close()
+
+    def handleSpectator(self,connexion,adresse):
+        try:
+            #TODO
+            print("")
+
+        except socket.error as e:
+            print(f"Erreur socket : {e}")
+
+        finally:
+            print("Fermeture des connexions...")
+            connexion.close()
+
+    def runGame(self):
+        pygame.init()
+        self.game = jeu.Jeu()
+        self.game.jouer()
+
+    def sendData(self, connexion, data):
+        game = pkl.dumps(data)
+        # Send size + actual data
+        size_prefix = struct.pack("I", len(game)) 
+        connexion.sendall(size_prefix + game)  
+        print(f"Envoi du jeu au client...")
+
 
 if __name__ == "__main__":
     serveur = server()
+    serveur.server()
