2f0cd9e22264d29c4ab0ff8e5613e5f9cb0b1399
ms-itachi <ismail.leomessi10@gmail.com>
2025-06-18 11:46:10 +0200

    change ip

--- reseau/client.py
+++ reseau/client.py
@@ -8,7 +8,7 @@ import requests
 import json
 
 class Client:
-    def __init__(self, ip_adress="172.20.10.10", port=4000, pin=13): 
+    def __init__(self, ip_adress="172.20.10.1", port=4000, pin=13): 
         self.cam = Matching()
         self.ip_adress = ip_adress
         self.port = port
